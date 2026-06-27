c777ac02f5ac22060c022e2128d483d49e41efe6
H-xiaoH <a412454922@gmail.com>
2026-06-27 18:43:02 +0800

    ✨ feat: 版本号使用日期格式 YYYYMMDD

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,8 @@
 import { updateDist } from '@gkd-kit/tools';
 import subscription from './check';
 
+const today = new Date();
+subscription.version =
+  today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+
 await updateDist(subscription);
