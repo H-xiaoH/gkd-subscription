afd7f3e6bf812b380607a56087aa426a8e9a8c43
H-xiaoH <a412454922@gmail.com>
2026-06-27 18:44:33 +0800

    🔧 chore: 更新 @gkd-kit/api 并改版本号格式为 YYMMDD

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,6 +3,6 @@ import subscription from './check';
 
 const today = new Date();
 subscription.version =
-  today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+  (today.getFullYear() % 100) * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
 
 await updateDist(subscription);
